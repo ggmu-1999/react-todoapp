4dc822197676518c8b2ce199043d798c30e89917
subhojit <goswamisubhojit1947@gmail.com>
2019-01-13 22:20:56 +0530

    todoapp

---
+++ src/Todos.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+const Todos = ({todos, deleteTodo}) => {
+    const todoList = todos.length ? (
+        todos.map(todo => {
+            return (
+                <div className="collection-item" key={todo.id}>
+                <span onClick={() => {deleteTodo(todo.id)}}>
+                    {todo.content}
+                </span>
+            </div>
+            )
+        })
+    ) : (
+        <div>
+            <p className="center">
+                You have no todo's left
+            </p>
+        </div>
+    )
+    
+    return (
+        <div className="todos collection">
+        {todoList}
+        </div>
+    )
+}
+
+export default Todos;
